43214effc81aaf4531aa7e18ae9a46d4c596bbe3
Burak Şahin <suspicioususer>
2017-06-07 18:34:21 +0300

    Decorator

---
+++ src/structural/decorator/ChineeseFood.java
@@ -0,0 +1,16 @@
+package structural.decorator;
+
+public class ChineeseFood extends FoodDecorator {
+	
+	public ChineeseFood(Food newFood) {
+		super(newFood);
+	}
+
+	public String prepareFood() {
+		return super.prepareFood() + " With Fried Rice and Manchurian  ";
+	}
+
+	public double foodPrice() {
+		return super.foodPrice() + 65.0;
+	}
+}
---
+++ src/structural/decorator/Food.java
@@ -0,0 +1,9 @@
+package structural.decorator;
+
+public interface Food {
+
+	public String prepareFood();
+
+	public double foodPrice();
+
+}
